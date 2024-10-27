e19e602923e008371ee19ff584c6795216294297
dishant <aryadishant414@gmail.com>
2024-10-27 08:44:30 +0530

    Working on data components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Hero from './../components/Hero';
 import {FloatingNav} from '../components/ui/FloatingNav'
 import { FaHome } from "react-icons/fa";
+import Grid from '../components/Grid'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
 
 
         <Hero />
+        <Grid />
       </div>
     </main>
   );
